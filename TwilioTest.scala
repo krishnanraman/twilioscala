7ea091cfb2c77029725a4e045005c290d8a4b69f
Krishnan Raman <kr61107@gmail.com>
2016-11-30 21:49:02 -0800

    Update TwilioTest.scala

--- TwilioTest.scala
+++ TwilioTest.scala
@@ -2,13 +2,17 @@
 // twilioscala.SMS(Auth(accountSid, authToken), SMS(toNumber, fromNumber, smsText))
 
 package twilioscala
-import com.twilio.rest.api.v2010.account.Message
-import com.twilio.Twilio
-import com.twilio.`type`.PhoneNumber
+
 case class Auth(account:String, token:String)
+
 case class SMS(toNumber:String, fromNumber:String, text:String)
+
 object SMS {
     def apply(auth:Auth, sms:SMS):String = {
+        import com.twilio.rest.api.v2010.account.Message
+        import com.twilio.Twilio
+        import com.twilio.`type`.PhoneNumber
+
         Twilio.init(auth.account, auth.token)
         val message = Message.creator(new PhoneNumber(sms.toNumber), 
                                       new PhoneNumber(sms.fromNumber), 
@@ -17,6 +21,7 @@ object SMS {
         message.getSid()
     }   
 }
+
 object TwilioTest extends App {
     val accountSid = "YOUR_ACCOUNT_SID"
     val authToken = "YOUR_AUTH_TOKEN"
